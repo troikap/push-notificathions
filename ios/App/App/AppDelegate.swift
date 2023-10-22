39d405fe66556360a8613244083f21ad78a92e45
Federico Gallardo <federico.gallardo@mecubro.com>
2023-10-22 01:04:31 -0300

    Modificacion proyecto iOS

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -1,5 +1,10 @@
 import UIKit
 import Capacitor
+import FirebaseCore
+import FirebaseAnalytics
+import FirebaseCrashlytics
+import FirebaseMessaging
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -8,6 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
