461b7765d37e1afe82e3fd8fb2d9de93e9b61ef7
Federico Gallardo <federico.gallardo@mecubro.com>
2023-10-23 16:07:11 -0300

    Sentry para pruebas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { RouteReuseStrategy, Router } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
@@ -10,7 +10,7 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { Authconfig } from './core/providers/interceptor/authconfig.provider';
 import { IonicStorageModule } from '@ionic/storage-angular';
 import { Drivers } from '@ionic/storage';
-
+import * as SentrySibling from "@sentry/angular-ivy";
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -26,6 +26,17 @@ import { Drivers } from '@ionic/storage';
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
     { provide: HTTP_INTERCEPTORS, useClass: Authconfig, multi: true },
+    { provide: ErrorHandler, useValue: SentrySibling.createErrorHandler() },
+    {
+      provide: SentrySibling.TraceService,
+      deps: [Router],
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => () => {},
+      deps: [SentrySibling.TraceService],
+      multi: true,
+    },
   ],
   bootstrap: [AppComponent],
 })
