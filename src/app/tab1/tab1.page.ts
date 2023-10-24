ab33b38993a52b3899a4e1615609bc473c01665f
lucasperezgracia <lucasperezgracia@gmail.com>
2023-10-24 10:44:48 -0300

    [Add]: loading agregados y time a 5s

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,6 +6,7 @@ import { ToastProvider } from '../core/providers/toast/toast.provider';
 import { ErrorProvider } from '../core/providers/error/error.provider';
 import { AlertController } from '@ionic/angular';
 import { Capacitor } from '@capacitor/core';
+import { LoadingProvider } from '../core/providers/loading/loading.provider';
 
 @Component({
   selector: 'app-tab1',
@@ -24,15 +25,18 @@ export class Tab1Page {
     private storageProvider: StorageProvider,
     private toastProvider: ToastProvider,
     private errorProvider: ErrorProvider,
-    private alertController: AlertController
+    private alertController: AlertController,
+    private loadingProvider: LoadingProvider
   ) {}
 
   async onClickSignUp() {
+    let loading = await this.loadingProvider.showLoading('Registrando dispositivo');
     try {
       await this.pushNotificationService.registerPush();
       setTimeout(async () => {
         const token = await this.storageProvider.getObject('TOKEN');
         if (!token) return this.toastProvider.presentToast('Problemas al obtener token', 3000, 'warning');
+        this.loadingProvider.closeLoader(loading);
         const alert = await this.alertController.create({
           header: 'Usuario',
           subHeader: 'Ingrese el nombre de su usuario',
@@ -58,6 +62,7 @@ export class Tab1Page {
         await alert.present();
         const alertResult = await alert.onDidDismiss();
         const userName = alertResult.data.values['name'];
+        loading = await this.loadingProvider.showLoading('Agregando dispositivo');
         const response = await this.genericService.setToken(token, userName, Capacitor.getPlatform());
         if (response) {
           this.toastProvider.presentToast('Token registrado correctamente', 3000, 'success');
@@ -66,14 +71,17 @@ export class Tab1Page {
             return element;
           });
         }
-      }, 1000);
+      }, 5000);
     } catch (err) {
       console.log('ERROR ', err);
       this.errorProvider.errorHandler(err);
+    } finally {
+      this.loadingProvider.closeLoader(loading);
     }
   }
 
   async onClickGetTokens() {
+    const loading = await this.loadingProvider.showLoading('Obteniendo tokens');
     try {
       const response = await this.genericService.getToken();
       if (response) {
@@ -82,22 +90,25 @@ export class Tab1Page {
           element.checked = false;
           return element;
         });
-        console.log('TOKKEN SETEADOS ', this.tokens);
-
       }
     } catch (err) {
       console.log('ERROR ', err);
       this.errorProvider.errorHandler(err);
+    } finally {
+      this.loadingProvider.closeLoader(loading);
     }
   }
 
   async onClickGetToken() {
+    const loading = await this.loadingProvider.showLoading('Obteniendo token propio');
     const token = await this.storageProvider.getObject('TOKEN');
     if (token) this.toastProvider.presentToast(`Tenes seteado el token ${token}`, 7000, 'success');
     else this.toastProvider.presentToast('No tenes seteado el token', 3000, 'warning');
+    this.loadingProvider.closeLoader(loading);
   }
 
   async onClickHello() {
+    const loading = await this.loadingProvider.showLoading('Probando funcionamiento api');
     try {
       const resposne = await this.genericService.getHello();
       await this.storageProvider.setObject('HELLO', 'STORAGE');
@@ -105,6 +116,8 @@ export class Tab1Page {
     } catch (err) {
       console.log('ERROR ', err);
       this.errorProvider.errorHandler(err);
+    } finally {
+      this.loadingProvider.closeLoader(loading);
     }
   }
 
@@ -119,13 +132,15 @@ export class Tab1Page {
       message: this.message,
       image: this.image
     }
+    const loading = await this.loadingProvider.showLoading('Enviando mensaje push');
     try {
       const response = await this.genericService.setMessage(payload);
       if (response) return this.toastProvider.presentToast('Mensaje enviado correctamente', 3000, 'success');
-      console.log('REPSONSE ', response);
     } catch (err) {
       console.log('ERROR ', err);
       this.errorProvider.errorHandler(err);
+    } finally {
+      this.loadingProvider.closeLoader(loading);
     }
   }
 
