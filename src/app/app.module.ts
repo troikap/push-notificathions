8361accc2dfb7af23ac0ded984cc0ddca000fa03
Federico Gallardo <federico.gallardo@mecubro.com>
2023-10-25 14:02:16 -0300

    Quitar sentry

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { APP_INITIALIZER, ErrorHandler, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy, Router } from '@angular/router';
 
@@ -10,7 +10,6 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { Authconfig } from './core/providers/interceptor/authconfig.provider';
 import { IonicStorageModule } from '@ionic/storage-angular';
 import { Drivers } from '@ionic/storage';
-import * as SentrySibling from "@sentry/angular-ivy";
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -26,17 +25,6 @@ import * as SentrySibling from "@sentry/angular-ivy";
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
     { provide: HTTP_INTERCEPTORS, useClass: Authconfig, multi: true },
-    { provide: ErrorHandler, useValue: SentrySibling.createErrorHandler() },
-    {
-      provide: SentrySibling.TraceService,
-      deps: [Router],
-    },
-    {
-      provide: APP_INITIALIZER,
-      useFactory: () => () => {},
-      deps: [SentrySibling.TraceService],
-      multi: true,
-    },
   ],
   bootstrap: [AppComponent],
 })
--- src/main.ts
+++ src/main.ts
@@ -3,40 +3,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import * as Sentry from "@sentry/capacitor";
-import * as SentrySibling from "@sentry/angular-ivy";
 
 if (environment.production) {
   enableProdMode();
 }
-
-Sentry.init({
-  dsn: "https://e7bea9ebf687aa1b1dce0bdc66effb91@o4506101050507264.ingest.sentry.io/4506101066170368",
-  // To set your release and dist versions
-  release: "push-notifications-troikap@" + "v1.0",
-  dist: "1",
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-  // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
-  // We recommend adjusting this value in production.
-  tracesSampleRate: 1.0,
-  integrations: [
-    new SentrySibling.BrowserTracing({
-      tracePropagationTargets: [
-        "localhost",
-        "https://g2f0hhh3-9000.brs.devtunnels.ms/api" ,
-      ],
-      routingInstrumentation: SentrySibling.routingInstrumentation,
-    }),
-    new SentrySibling.Replay({
-      maskAllText: true,
-      blockAllMedia: true,
-    })
-  ],
-},
-// Forward the init method to the sibling Framework.
-SentrySibling.init
-);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
